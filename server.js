4756c1adbc77422b97d83c2e8243837a86ea2f08
Sumit Sahu <sumitkumarsahu141@gmail.com>
2024-02-27 00:32:41 +0530

    'firstcommit'

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const path = require('path')
+const expressLayout = require('express-ejs-layouts')
+const PORT = process.env.PORT || 3000
+
+
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+
+
+
+
+// set template engine
+app.use(expressLayout)
+app.set('views', path.join(__dirname,'/resources/views')) // if only views folder in differnt path
+app.set('view engine', 'ejs') // required to set  ejs , pug different types of template engine
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Listening on port ${PORT}`);
+})
+
